6939e4c9c0b7f785147a0e821c55394b3132f85c
jainegi <jainegi645@gmail.com>
2022-04-13 22:17:51 +0530

    setup cloudinary acc.

--- config/plugins.js
+++ config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'cloudinary',
         providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME','dvofl7mgy'),
-          api_key: env('CLOUDINARY_KEY','263851665987895'),
-          api_secret: env('CLOUDINARY_SECRET','RHaO5f11-AwGAilED_H2orQazOU'),
+          cloud_name: env('CLOUDINARY_NAME','damaczg5v'),
+          api_key: env('CLOUDINARY_KEY','329985189747586'),
+          api_secret: env('CLOUDINARY_SECRET','4SkMntAGaN9CeKMnNKk7X5S9cY4'),
         },
         actionOptions: {
           upload: {},
